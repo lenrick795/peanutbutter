e46b0fadfa2f212fb12de48d9a7fa9d53cfd4033
Ricardo Sabates <ricardo.sabates@lennar.com>
2025-04-16 10:13:00 -0400

    more lab work

--- modules/eks-auth/main.tf
+++ modules/eks-auth/main.tf
@@ -1,12 +1,12 @@
 resource "aws_eks_access_entry" "main" {
   cluster_name  = var.eks_cluster.cluster_name
-  principal_arn = var.aws_eks_access_entry_principal_arn
+  principal_arn = var.principal_arn
 }
 
 resource "aws_eks_access_policy_association" "main" {
   cluster_name  = var.eks_cluster.cluster_name
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = var.aws_eks_access_entry_principal_arn
+  principal_arn = var.principal_arn
 
   access_scope {
     type = "cluster"
